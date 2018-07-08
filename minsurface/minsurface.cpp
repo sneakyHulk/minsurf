24fb606fa45b15150cba8db3f1cf248f21319b84
Lukas Heyn <lukas.heyn@gmail.com>
2018-07-08 19:19:06 +0200

    fgsdfg

---
+++ minsurface/minsurface.cpp
@@ -0,0 +1,12 @@
+﻿// minsurface.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "minsurface.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
