d6fc94742b3fa6a2e5c03b2a512cd567463ed0fe
Lukas Heyn <lukas.heyn@gmail.com>
2018-07-20 11:32:21 +0200

    brewfbourfou

---
+++ src/DISCRETIZATIONS.h
@@ -0,0 +1,450 @@
+#pragma once
+
+#include "Eigen/SparseCore"
+#include "Eigen/IterativeLinearSolvers"
+
+#include "DERIVATIVE_CORE.h"
+#include "DISCRETIZATION_CORE.h"
+#include "SYSTEM_MATRIX_CORE.h"
+#include "RESIDUAL_CORE.h"
+
+template <class T>
+class NEWTON : public DISCRETIZATION_CORE<T>, public RESIDUAL_3x3<T>, public DERIVATIVE_HAND_3x3<T>, public JACOBIAN_HARD_3x3<T> {
+protected:
+	using DISCRETIZATION_CORE<T>::h; using DISCRETIZATION_CORE<T>::m; using DISCRETIZATION_CORE<T>::n; using DISCRETIZATION_CORE<T>::s;
+	typedef Eigen::SparseMatrix<T, Eigen::RowMajor> MT;
+	typedef Eigen::Matrix<T, Eigen::Dynamic, 1> VT;
+	MT JR;
+	VT R, Z, X, DR;
+	T norm_Fz_prev/*||F(z[k-1])||*/, norm_Fz_JRX_prev/*||F(z[k-1]) - F'(z[k-1]) * X[k-1]||*/;
+	T ny, ny_prev, ny_max;
+	T epsilon, delta;
+	int max_newton_iterations, k_tilde, safety_output;
+
+	NEWTON(std::vector<std::string>& input) : DISCRETIZATION_CORE<T>(input), RESIDUAL_3x3<T>(h()), DERIVATIVE_HAND_3x3<T>(h()), JACOBIAN_HARD_3x3<T>(m()), ny(0.1), norm_Fz_JRX_prev(0.0), norm_Fz_prev(0.0), ny_prev(0.0), ny_max(0.9) {
+		Z = Eigen::Array<T, Eigen::Dynamic, 1>::Constant(n(), DISCRETIZATION_CORE<T>::calculate_arithmetic_mean());
+
+		start();
+	}
+	NEWTON(std::vector<std::string>& input, char * vtk_filename) : DISCRETIZATION_CORE<T>(input, vtk_filename), RESIDUAL_3x3<T>(h()), DERIVATIVE_HAND_3x3<T>(h()), JACOBIAN_HARD_3x3<T>(m()), ny(0.1), norm_Fz_JRX_prev(0.0), norm_Fz_prev(0.0), ny_prev(0.0), ny_max(0.9) {
+		Z.resize(n());
+
+#pragma omp parallel for
+		for (unsigned int i = 0; i < m(); i++) {
+			for (unsigned int j = 0; j < m(); j++) {
+				Z(m()*i + j) = DISCRETIZATION_CORE<T>::surface()[i][j];
+			}
+		}
+
+		start();
+	}
+	NEWTON(DISCRETIZATION_CORE<T>* dis, VT Z) : DISCRETIZATION_CORE<T>(*dis), RESIDUAL_3x3<T>(h()), DERIVATIVE_HAND_3x3<T>(h()), JACOBIAN_HARD_3x3<T>(m()), Z(Z), ny(0.1), norm_Fz_JRX_prev(0.0), norm_Fz_prev(0.0), ny_prev(0.0), ny_max(0.9) {
+		start();
+	}
+
+	void start() {
+		max_newton_iterations = test_on_input_value<int>("MAX NEWTON ITERATIONS", 8, DISCRETIZATION_CORE<T>::input()); // TEST ON MAX NEWTON ITERATIONS
+		safety_output = test_on_input_value<int>("NEWTON ITERATIONS PER OUTPUT", 9, DISCRETIZATION_CORE<T>::input()); // TEST ON NEWTON ITERATIONS PER OUTPUT
+		epsilon = test_on_input_value<T>("EPSILON", 10, DISCRETIZATION_CORE<T>::input()); // TEST ON EPSILON
+		delta = test_on_input_value<T>("DELTA", 11, DISCRETIZATION_CORE<T>::input()); // TEST ON DELTA
+		k_tilde = test_on_input_value<int>("K_TILDE", 12, DISCRETIZATION_CORE<T>::input()); // TEST ON K_TILDE
+
+		JR.resize(n(), n());
+		JR.reserve(Eigen::Matrix<T, Eigen::Dynamic, 1>::Constant(n(), 9));
+		R.resize(n());
+		X.resize(n());
+		DR.resize(n());
+	}
+
+	//GETTER-METHODS:
+	virtual std::vector<T> getSTENCILS(unsigned int i) { return DISCRETIZATION_CORE<T>::stencils()[i]; }
+	virtual T getZ(unsigned int i) { return Z(i); }
+	virtual bool stop() { return (DISCRETIZATION_CORE<T>::count()++ <= max_newton_iterations && (DR.norm() / n()) > epsilon); }
+
+	//SETTER-METHODS:
+	virtual void insertRESIDUAL(unsigned int i, T value) { R(i) = value; }
+	virtual void insertSYSTEM_MATRIX(unsigned int i, unsigned int j, T value) { JR.coeffRef(i, j) = value; }
+
+	//CALCULATION-METHODS:
+	virtual std::vector<T> calculateSTENCIL(unsigned int i, unsigned int j) {
+		return { DISCRETIZATION_CORE<T>::surface()[i][j],  DISCRETIZATION_CORE<T>::surface()[i][j + 1], DISCRETIZATION_CORE<T>::surface()[i][j + 2],
+			DISCRETIZATION_CORE<T>::surface()[i + 1][j], DISCRETIZATION_CORE<T>::surface()[i + 1][j + 1], DISCRETIZATION_CORE<T>::surface()[i + 1][j + 2],
+			DISCRETIZATION_CORE<T>::surface()[i + 2][j], DISCRETIZATION_CORE<T>::surface()[i + 2][j + 1], DISCRETIZATION_CORE<T>::surface()[i + 2][j + 2] };
+	}
+	virtual T calculateRESIDUAL(const std::vector<T> & stencil) { return RESIDUAL_3x3<T>::calculateRESIDUAL(stencil); }
+	virtual T calculateDERIVATIVE(unsigned int x, unsigned int derivative_to) { return DERIVATIVE_HAND_3x3<T>::calculateDERIVATIVE(x, derivative_to); }
+	virtual void solve() = 0;
+	void updateDRhelper() {
+#pragma omp parallel for
+		for (int i = 0; i < m(); i++) {
+			for (unsigned int j = 0; j < m(); j++) {
+				unsigned int pos = i * m() + j;
+				std::vector<T> stencil = calculateSTENCIL(i, j);
+				DISCRETIZATION_CORE<T>::stencils()[pos] = stencil;
+				DR(pos) = calculateRESIDUAL(stencil);
+			}
+		}
+	}
+	void updateDR() {
+#pragma omp parallel for
+		for (int i = 1; i < s() - 1; i++) {
+			for (unsigned int j = 1; j < s() - 1; j++) {
+				unsigned int pos = (i - 1) * m() + (j - 1);
+				DISCRETIZATION_CORE<T>::surface()[i][j] = Z(pos) + X(pos);
+			}
+		}
+		updateDRhelper();
+	}
+	void output() {
+		std::cout << "NEWTON ITERATION: " << DISCRETIZATION_CORE<T>::count() << ", " << "SCALED ERROR: '" << (DR.norm() / n()) << "', OUTPUTTING ." << std::flush;
+		double timestamp = omp_get_wtime();
+		DISCRETIZATION_CORE<T>::output();
+		std::cout << ".. TOOK (" << omp_get_wtime() - timestamp << "s)" << std::endl;
+	}
+	void calculateNY() {
+		//ny: Forcing term for linear system solver
+		ny = (R.norm() - norm_Fz_JRX_prev) / norm_Fz_prev;
+		ny < 0 ? ny = ny * (-1.) : 0;
+		T temp = pow(ny_prev, (1. + sqrt(5.)) / 2.);
+		(ny < temp) && (temp > 0.1) ? ny = temp : 0;
+		ny > ny_max ? ny = ny_max : 0;
+	}
+	void updateNY_calculateK() {
+		norm_Fz_prev = R.norm();
+		norm_Fz_JRX_prev = (R + JR * X).norm();
+		ny_prev = ny;
+
+		static bool ignore_warnings = false;
+		if (!ignore_warnings) {
+			static int k = 0;
+			if ((DR - R).norm() / DR.norm() < delta) {
+				k = k + 1;
+			}
+			else {
+				k = 0;
+			}
+			if (!(k < k_tilde)) {
+				std::string tmp;
+				std::cout << "SLOW CONVERGENCE!, CONTINUE? (y/n) ";
+				std::cin >> tmp;
+				while (tmp != "y" && tmp != "n") {
+					std::cout << "SLOW CONVERGENCE!, CONTINUE? (y/n) ";
+					std::cin >> tmp;
+				}
+				if (tmp == "n") {
+					std::cout << "LAST "; NEWTON<T>::output();
+					throw std::string("STOPPING NEWTON BECAUSE OF SLOW CONVERGENCE!");
+				}
+
+				ignore_warnings = true;
+			}
+		}
+	}
+	void lin_solve() {
+		Eigen::BiCGSTAB<MT> solver;
+		solver.setTolerance(ny);
+		solver.compute(JR);
+		X = solver.solve((-1)*R);
+		if (X != X) {
+			std::cout << "LAST "; NEWTON<T>::output();
+			throw std::string("STOPPING NEWTON BECAUSE OF NON CONVERGENCE! TRY DIFFERENT BOUNDARY VALUES OR METHOD!");
+		}
+	}
+};
+
+template <class T>
+class NEWTON_CLASSIC : public NEWTON<T> {
+	using DISCRETIZATION_CORE<T>::m; using DISCRETIZATION_CORE<T>::s;
+	using NEWTON<T>::JR; using NEWTON<T>::Z; using NEWTON<T>::X; using NEWTON<T>::R; using NEWTON<T>::DR;
+	using NEWTON<T>::norm_Fz_prev; using NEWTON<T>::norm_Fz_JRX_prev;
+	using NEWTON<T>::ny; using NEWTON<T>::ny_prev; using NEWTON<T>::ny_max;
+	using NEWTON<T>::epsilon; using NEWTON<T>::delta;
+	using NEWTON<T>::max_newton_iterations;	using NEWTON<T>::k_tilde; using NEWTON<T>::safety_output;
+	typedef Eigen::SparseMatrix<T, Eigen::RowMajor> MT;
+	typedef Eigen::Matrix<T, Eigen::Dynamic, 1> VT;
+public:
+	NEWTON_CLASSIC(std::vector<std::string>& input) : NEWTON<T>(input) { start(); }
+	NEWTON_CLASSIC(std::vector<std::string>& input, char * vtk_filename) : NEWTON<T>(input, vtk_filename) { start(); }
+	NEWTON_CLASSIC(DISCRETIZATION_CORE<T>* dis, VT Z) : NEWTON<T>(dis, Z) { start(); }
+private:
+	void start() {
+		DISCRETIZATION_CORE<T>::solve_core(); std::cout << "SUCCESS! - LAST "; NEWTON<T>::output(); 
+	}
+	virtual void solve() {
+		static bool first_iter = true;
+
+		if (DISCRETIZATION_CORE<T>::count() % safety_output == 0) {
+			NEWTON<T>::output();
+		}
+
+		JACOBIAN_HARD_3x3<T>::calcualteSYSTEM_MATRIX(DISCRETIZATION_CORE<T>::surface());
+
+		if (!first_iter) {
+			NEWTON<T>::calculateNY();
+		}
+
+		NEWTON<T>::lin_solve();
+		
+		NEWTON<T>::updateDR();
+		Z = Z + X;
+
+		NEWTON<T>::updateNY_calculateK();
+
+		first_iter = false;
+	}
+};
+
+template <class T>
+class NEWTON_DAMPED_TRIVIAL : public NEWTON<T> {
+	using DISCRETIZATION_CORE<T>::m; using DISCRETIZATION_CORE<T>::s;
+	using NEWTON<T>::JR; using NEWTON<T>::Z; using NEWTON<T>::X; using NEWTON<T>::R; using NEWTON<T>::DR;
+	using NEWTON<T>::norm_Fz_prev; using NEWTON<T>::norm_Fz_JRX_prev;
+	using NEWTON<T>::ny; using NEWTON<T>::ny_prev; using NEWTON<T>::ny_max;
+	using NEWTON<T>::epsilon; using NEWTON<T>::delta;
+	using NEWTON<T>::max_newton_iterations;	using NEWTON<T>::k_tilde; using NEWTON<T>::safety_output;
+	typedef Eigen::SparseMatrix<T, Eigen::RowMajor> MT;
+	typedef Eigen::Matrix<T, Eigen::Dynamic, 1> VT;
+	T lambda, min_lambda;
+public:
+	NEWTON_DAMPED_TRIVIAL(std::vector<std::string>& input) : NEWTON<T>(input), lambda(1) { start(); }
+	NEWTON_DAMPED_TRIVIAL(std::vector<std::string>& input, char * vtk_filename) : NEWTON<T>(input, vtk_filename), lambda(1) { start(); }
+	NEWTON_DAMPED_TRIVIAL(DISCRETIZATION_CORE<T>* dis, VT Z) : NEWTON<T>(dis, Z), lambda(1) { start(); }
+private:
+	void start() { 
+		min_lambda = test_on_input_value<T>("MIN LAMBDA", 13, DISCRETIZATION_CORE<T>::input()); // TEST ON MIN LAMBDA
+		DISCRETIZATION_CORE<T>::solve_core(); std::cout << "SUCCESS! - LAST "; NEWTON<T>::output();
+	}
+	virtual void solve() {
+		static bool first_iter = true;
+
+		if (DISCRETIZATION_CORE<T>::count() % safety_output == 0) {
+			NEWTON<T>::output();
+		}
+
+		JACOBIAN_HARD_3x3<T>::calcualteSYSTEM_MATRIX(DISCRETIZATION_CORE<T>::surface());
+
+		if (!first_iter) {
+			NEWTON<T>::calculateNY();
+		}
+
+		NEWTON<T>::lin_solve();
+
+		if (first_iter) {
+			DAMPER();
+		}
+		else {
+			if (R.norm() > DR.norm()) {
+				Z = Z + lambda * X;
+				if (lambda < 1) {
+					lambda = 2 * lambda;
+				}
+			}
+			else {
+				DAMPER();
+			}
+		}
+
+		NEWTON<T>::updateNY_calculateK();
+		
+		first_iter = false;
+	}
+
+	void DAMPER() {
+		do {
+			updateDR();
+
+			lambda = lambda / 2;
+		} while (R.norm() < DR.norm() && 2 * lambda > min_lambda);
+
+		lambda = 2 * lambda;
+		Z = Z + lambda * X;
+	}
+
+	void updateDR() {
+#pragma omp parallel for
+		for (int i = 1; i < s() - 1; i++) {
+			for (unsigned int j = 1; j < s() - 1; j++) {
+				unsigned int pos = (i - 1) * m() + (j - 1);
+				DISCRETIZATION_CORE<T>::surface()[i][j] = Z(pos) + lambda * X(pos);
+			}
+		}
+		NEWTON<T>::updateDRhelper();
+	}
+};
+
+template <class T>
+class NEWTON_DAMPED_BACKTRACKING : public NEWTON<T> {
+	using DISCRETIZATION_CORE<T>::n; using DISCRETIZATION_CORE<T>::m; using DISCRETIZATION_CORE<T>::s;
+	using NEWTON<T>::JR; using NEWTON<T>::Z; using NEWTON<T>::X; using NEWTON<T>::R; using NEWTON<T>::DR;
+	using NEWTON<T>::norm_Fz_prev; using NEWTON<T>::norm_Fz_JRX_prev;
+	using NEWTON<T>::ny; using NEWTON<T>::ny_prev; using NEWTON<T>::ny_max;
+	using NEWTON<T>::epsilon; using NEWTON<T>::delta;
+	using NEWTON<T>::max_newton_iterations;	using NEWTON<T>::k_tilde; using NEWTON<T>::safety_output;
+	typedef Eigen::SparseMatrix<T, Eigen::RowMajor> MT;
+	typedef Eigen::Matrix<T, Eigen::Dynamic, 1> VT;
+	VT DReps, DR0;
+	T t, thetha, thetha_max, thetha_min;
+public:
+	NEWTON_DAMPED_BACKTRACKING(std::vector<std::string>& input) : NEWTON<T>(input), t(0.0001), thetha(1.), thetha_min(0.1), thetha_max(0.5) { start(); }
+	NEWTON_DAMPED_BACKTRACKING(std::vector<std::string>& input, char * vtk_filename) : NEWTON<T>(input, vtk_filename), t(0.0001), thetha(1.), thetha_min(0.1), thetha_max(0.5) { start(); }
+	NEWTON_DAMPED_BACKTRACKING(DISCRETIZATION_CORE<T>* dis, VT Z) : NEWTON<T>(dis, Z), t(0.0001), thetha(1.), thetha_min(0.1), thetha_max(0.5) { start(); }
+private:
+	void start() {
+		DReps.resize(n());
+		DR0.resize(n());
+		DISCRETIZATION_CORE<T>::solve_core(); std::cout << "SUCCESS! - LAST "; NEWTON<T>::output();
+	}
+	virtual void solve() {
+		static bool first_iter = true;
+
+		if (DISCRETIZATION_CORE<T>::count() % safety_output == 0) {
+			NEWTON<T>::output();
+		}
+
+		JACOBIAN_HARD_3x3<T>::calcualteSYSTEM_MATRIX(DISCRETIZATION_CORE<T>::surface());
+
+		if (!first_iter) {
+			NEWTON<T>::calculateNY();
+		}
+
+		NEWTON<T>::lin_solve();
+
+		NEWTON<T>::updateDR();
+		T p_0 = R.squaredNorm() / 2.;
+		while (DR.norm() > ((1. - t * (1. - ny)) * R.norm())) {
+			updateDReps();
+			updateDR0();
+			T p_prime_0 = (DReps.squaredNorm() - DR0.squaredNorm()) / (2. * 0.000001);
+			T p_1 = DR.squaredNorm() / 2.;
+			thetha = -p_prime_0 / (2. * (p_1 - p_0 - p_prime_0));
+			if (thetha < thetha_min) { thetha = thetha_min; }
+			if (thetha > thetha_max) { thetha = thetha_max; }
+			X = thetha * X;
+			ny = 1. - thetha * (1. - ny);
+			NEWTON<T>::updateDR();
+		}
+		Z = Z + X;
+
+		NEWTON<T>::updateNY_calculateK();
+
+		first_iter = false;
+	}
+
+	void updateDReps() {
+#pragma omp parallel for
+		for (int i = 1; i < s() - 1; i++) {
+			for (unsigned int j = 1; j < s() - 1; j++) {
+				unsigned int pos = (i - 1) * m() + (j - 1);
+				DISCRETIZATION_CORE<T>::surface()[i][j] = Z(pos) + 0.000001 * X(pos);
+			}
+		}
+#pragma omp parallel for
+		for (int i = 0; i < m(); i++) {
+			for (unsigned int j = 0; j < m(); j++) {
+				unsigned int pos = i * m() + j;
+				std::vector<T> stencil = NEWTON<T>::calculateSTENCIL(i, j);
+				DISCRETIZATION_CORE<T>::stencils()[pos] = stencil;
+				DReps(pos) = NEWTON<T>::calculateRESIDUAL(stencil);
+			}
+		}
+	}
+	void updateDR0() {
+#pragma omp parallel for
+		for (int i = 1; i < s() - 1; i++) {
+			for (unsigned int j = 1; j < s() - 1; j++) {
+				unsigned int pos = (i - 1) * m() + (j - 1);
+				DISCRETIZATION_CORE<T>::surface()[i][j] = Z(pos);
+			}
+		}
+#pragma omp parallel for
+		for (int i = 0; i < m(); i++) {
+			for (unsigned int j = 0; j < m(); j++) {
+				unsigned int pos = i * m() + j;
+				std::vector<T> stencil = NEWTON<T>::calculateSTENCIL(i, j);
+				DISCRETIZATION_CORE<T>::stencils()[pos] = stencil;
+				DR0(pos) = NEWTON<T>::calculateRESIDUAL(stencil);
+			}
+		}
+	}
+};
+
+template <class T>
+class LAPLACE : public DISCRETIZATION_CORE<T>, public RESIDUAL_STAR<T>, public LAPLACIAN_OPERATOR<T> {
+	using DISCRETIZATION_CORE<T>::h; using DISCRETIZATION_CORE<T>::m; using DISCRETIZATION_CORE<T>::n; using DISCRETIZATION_CORE<T>::s;
+	typedef Eigen::SparseMatrix<T, Eigen::RowMajor> MT;
+	typedef Eigen::Matrix<T, Eigen::Dynamic, 1> VT;
+	MT A;
+	VT R, Z, X;
+public:
+	LAPLACE(std::vector<std::string>& input) : DISCRETIZATION_CORE<T>(input), RESIDUAL_STAR<T>(h()), LAPLACIAN_OPERATOR<T>(m()) {
+		A.resize(n(), n());
+		A.reserve(Eigen::Matrix<T, Eigen::Dynamic, 1>::Constant(n(), 5));
+		R.resize(n());
+		X.resize(n());
+		Z.resize(n());
+
+		DISCRETIZATION_CORE<T>::solve_core();
+
+		if (DISCRETIZATION_CORE<T>::input()[0].size() == 1) {
+			std::cout << "OUTPUTTING ." << std::flush;
+			double timestamp = omp_get_wtime();
+			DISCRETIZATION_CORE<T>::output();
+			std::cout << ".. TOOK (" << omp_get_wtime() - timestamp << "s)" << std::endl;
+			return;
+		}
+		if (!std::isdigit(static_cast<unsigned char>(DISCRETIZATION_CORE<T>::input()[0][1]))) {
+			throw std::string("SELECTION DIGIT FOR SOLVER IS NON DIGIT!");
+		}
+		int D = std::stoi(DISCRETIZATION_CORE<T>::input()[0].substr(1, DISCRETIZATION_CORE<T>::input()[0].size() - 1));
+		if (D == 2) {
+			NEWTON_CLASSIC<T>(static_cast<DISCRETIZATION_CORE<T>*>(this), Z);
+		}
+		/*else if (D == 1) {
+		DISCRETIZATION_3x3_SOLVER_EIGEN_JACOBIAN_HARD_DERIVATIVE_DIFFERENCEQUOTIENT<T>(static_cast<DISCRETIZATION_CORE<T>*>(this), Z);
+		}*/
+		else if (D == 3) {
+			NEWTON_DAMPED_TRIVIAL<T>(static_cast<DISCRETIZATION_CORE<T>*>(this), Z);
+		}
+		else if (D == 4) {
+			NEWTON_DAMPED_BACKTRACKING<T>(static_cast<DISCRETIZATION_CORE<T>*>(this), Z);
+		}
+		else {
+			std::stringstream tmp;
+			tmp << "DISCRETIZATION CORE '" << D << "' IS NOT SUPPORTED!";
+			throw tmp.str();
+		}
+	}
+private:
+	//GETTER-METHODS:
+	virtual std::vector<T> getSTENCILS(unsigned int i) { return DISCRETIZATION_CORE<T>::stencils()[i]; }
+	virtual T getZ(unsigned int i) { return Z(i); }
+	virtual bool stop() { return false; }
+
+	//SETTER-METHODS:
+	virtual void insertRESIDUAL(unsigned int i, T value) { R(i) = value; }
+	virtual void insertSYSTEM_MATRIX(unsigned int i, unsigned int j, T value) { A.coeffRef(i, j) = value; }
+
+	//CALCULATION-METHODS:
+	virtual std::vector<T> calculateSTENCIL(unsigned int i, unsigned int j) {
+		return { DISCRETIZATION_CORE<T>::surface()[i][j + 1],
+			DISCRETIZATION_CORE<T>::surface()[i + 1][j], DISCRETIZATION_CORE<T>::surface()[i + 1][j + 1], DISCRETIZATION_CORE<T>::surface()[i + 1][j + 2],
+			DISCRETIZATION_CORE<T>::surface()[i + 2][j + 1] };
+	}
+	virtual T calculateRESIDUAL(const std::vector<T> & stencil) { return RESIDUAL_STAR<T>::calculateRESIDUAL(stencil); }
+	virtual void solve() {
+		LAPLACIAN_OPERATOR<T>::calcualteSYSTEM_MATRIX(DISCRETIZATION_CORE<T>::surface());
+
+		A *= 1 / (h() * h());
+
+		Eigen::BiCGSTAB<MT> solver;
+		solver.setTolerance(10e-7);
+		solver.compute((-1)*A);
+		X = solver.solve(R);
+		if (X != X) {
+			DISCRETIZATION_CORE<T>::output();
+			throw std::string("STOPPING LAPLACE BECAUSE OF NON CONVERGENCE! TRY DIFFERENT BOUNDARY VALUES OR METHOD!");
+		}
+
+		Z = X;
+	}
+};
---
+++ src/DISCRETIZATION_CORE.h
@@ -0,0 +1,359 @@
+#pragma once
+
+#define VTK
+
+#ifdef VTK
+#include <vtkVersion.h>
+#include <vtkSmartPointer.h>
+#include <vtkPolyData.h>
+#include <vtkPoints.h>
+#include <vtkCellArray.h>
+#include <vtkXMLPolyDataWriter.h>
+#include <vtkXMLPolyDataReader.h>
+#include <vtkPointData.h>
+#include <vtkPolyDataNormals.h>
+#include <vtkDoubleArray.h>
+#include <vtkCellData.h>
+#endif // VTK
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <omp.h>
+#include <typeinfo>
+
+#include "INTERPRETER.h"
+
+template<class D>
+D test_on_input_value(const char * what, unsigned int pos_in_input, const std::vector<std::string>& input) {
+	D result;
+	try {
+		if (input.size() <= pos_in_input) {
+			std::stringstream tmp;
+			tmp << what << " NOT ENTERED!";
+			throw tmp.str();
+		}
+		if (std::is_same<D, int>::value) {
+			result = std::stoi(input[pos_in_input]);
+		}
+		else if (std::is_same<D, float>::value) {
+			result = std::stof(input[pos_in_input]);
+		}
+		else if (std::is_same<D, double>::value) {
+			result = std::stod(input[pos_in_input]);
+		}
+		else if (std::is_same<D, long double>::value) {
+			result = std::stold(input[pos_in_input]);
+		}
+		else {
+			std::stringstream tmp;
+			tmp << "DATA TYPE '" << typeid(D).name() << "' IS NOT SUPPORTED!" << std::endl;
+			throw tmp.str();
+		}
+		if (!(result > 0)) {
+			std::stringstream tmp;
+			tmp << what << " HAS TO BE LARGER THAN 0, IS: '" << result << "'!";
+			throw tmp.str();
+		}
+	}
+	catch (std::invalid_argument e) {
+		std::stringstream tmp;
+		tmp << "ERROR AT CONVERTING TO '" << typeid(D).name() << "' FOR " << what << " '" << e.what() << "'!";
+		throw tmp.str();
+	}
+	catch (std::out_of_range e) {
+		std::stringstream tmp;
+		tmp << "ERROR AT CONVERTING TO '" << typeid(D).name() << "' FOR " << what << " '" << e.what() << "'!";
+		throw tmp.str();
+	}
+	return result;
+}
+
+template <class T>
+class  DISCRETIZATION_CORE {
+	int _s;
+	unsigned int _n;
+	unsigned int _m;
+	T _h;
+	std::vector<std::vector<T>> _surface;
+	std::vector<std::vector<T>> _stencils;
+	std::string _path;
+	std::vector<std::string> _input;
+	unsigned int _count;
+
+protected:
+	DISCRETIZATION_CORE(std::vector<std::string>& input) : _input(input), _count(1) {
+		_s = test_on_input_value<int>("GRID POINTS PER ROW", 2, _input); //GRID-POINTS PER ROW
+		_h = 1. / (_s - 1);
+		_m = _s - 2;
+		_n = _m * _m;
+
+		//OUTPUT-PATH:
+		if (_input.size() <= 3) {
+			std::stringstream tmp;
+			tmp << "OUTPUT-PATH NOT ENTERED!";
+			throw tmp.str();
+		}
+		_path = _input[3];
+		//NORTH:	4*x^4-6*x^3+3*x^2-x
+		std::cout << "NORTH-";
+		if (_input.size() <= 4) {
+			std::stringstream tmp;
+			tmp << "NORTH FUNCTION NOT ENTERED!";
+			throw tmp.str();
+		}
+		INTERPRETER north = INTERPRETER(_input[4]);
+		//EAST:		sin(3.1415*x^(1/2))
+		std::cout << "EAST-";
+		if (_input.size() <= 5) {
+			std::stringstream tmp;
+			tmp << "EAST FUNCTION NOT ENTERED!";
+			throw tmp.str();
+		}
+		INTERPRETER east = INTERPRETER(_input[5]);
+		//SOUTH:	x^2
+		std::cout << "SOUTH-";
+		if (_input.size() <= 6) {
+			std::stringstream tmp;
+			tmp << "SOUTH FUNCTION NOT ENTERED!";
+			throw tmp.str();
+		}
+		INTERPRETER south = INTERPRETER(_input[6]);
+		//WEST:		arctan(0.9*x)+1-1.733*x^3
+		std::cout << "WEST-";
+		if (_input.size() <= 7) {
+			std::stringstream tmp;
+			tmp << "WEST FUNCTION NOT ENTERED!";
+			throw tmp.str();
+		}
+		INTERPRETER west = INTERPRETER(_input[7]);
+		
+		_surface.resize(_s);
+		for (unsigned int i = 0; i < _s; i++) {
+			_surface[i].resize(_s);
+		}
+		_stencils.resize(_n);
+		for (unsigned int i = 0; i < (_s /*- 1*/); i++) {
+			_surface[0][i] = north.calculate({ i * 1.0 / (_s - 1) });
+			_surface[i][_s - 1] = east.calculate({ i * 1.0 / (_s - 1) });
+			_surface[_s - 1][_s - 1 - i] = south.calculate({ i * 1.0 / (_s - 1) });
+			_surface[_s - 1 - i][0] = west.calculate({ i * 1.0 / (_s - 1) });
+		}
+
+		double north_west = std::abs(north.calculate({ 0 }) - west.calculate({ 1 }));
+		double north_east = std::abs(north.calculate({ 1 }) - east.calculate({ 0 }));
+		double south_east = std::abs(south.calculate({ 0 }) - east.calculate({ 1 }));
+		double south_west = std::abs(south.calculate({ 1 }) - west.calculate({ 0 }));
+
+		if (north_west > 0.1) { std::cout << "BOUNDARY VALUES IN THE NORTH-WEST CORNER DO NOT MATCH! TOTAL DIFFERENCE: " << north_west << std::endl; }
+		if (north_east > 0.1) { std::cout << "BOUNDARY VALUES IN THE NORTH-EAST CORNER DO NOT MATCH! TOTAL DIFFERENCE: " << north_east << std::endl; }
+		if (south_east > 0.1) { std::cout << "BOUNDARY VALUES IN THE SOUTH-EAST CORNER DO NOT MATCH! TOTAL DIFFERENCE: " << south_east << std::endl; }
+		if (south_west > 0.1) { std::cout << "BOUNDARY VALUES IN THE SOUTH-WEST CORNER DO NOT MATCH! TOTAL DIFFERENCE: " << south_west << std::endl; }
+
+		if (north_west > 0.1 || north_east > 0.1 || south_east > 0.1 || south_west > 0.1) {
+			std::string tmp;
+			std::cout << "CONTINUE? (y/n) ";
+			std::cin >> tmp;
+			while (tmp != "y" && tmp != "n") {
+				std::cout << "CONTINUE? (y/n) ";
+				std::cin >> tmp;
+			}
+			if (tmp == "n") {
+				throw std::string("BREAK!");
+			}
+		}
+	}
+
+	DISCRETIZATION_CORE(std::vector<std::string>& input, char * vtk_filename) : _input(input), _count(1) {
+		//OUTPUT-PATH:
+		if (_input.size() <= 3) {
+			std::stringstream tmp;
+			tmp << "OUTPUT-PATH NOT ENTERED!";
+			throw tmp.str();
+		}
+		_path = _input[3];
+		read_vtp(vtk_filename);
+		_h = 1. / (_s - 1);
+		_m = _s - 2;
+		_n = _m * _m;
+		_stencils.resize(_n);
+	}
+
+	//GETTER-METHODS:
+	unsigned int s() { return _s; }
+	unsigned int n() { return _n; }
+	unsigned int m() { return _m; }
+	T h() { return _h; }
+	std::vector<std::vector<T>>& surface() { return _surface; }
+	std::vector<std::vector<T>>& stencils() { return _stencils; }
+	std::vector<std::string>& input() { return _input; }
+	unsigned int& count() { return _count; }
+	virtual T getZ(unsigned int i) = 0;
+	virtual bool stop() = 0;
+
+	//SETTER-METHODS:
+	virtual void insertRESIDUAL(unsigned int i, T value) = 0;
+
+	//CALCULATION-METHODS:
+	virtual std::vector<T> calculateSTENCIL(unsigned int i, unsigned int j) = 0;
+	virtual T calculateRESIDUAL(const std::vector<T>& stencil) = 0;
+	virtual void solve() = 0;
+	void solve_core() {
+		do {
+			updateSurface();
+
+#pragma omp parallel for
+			for (int i = 0; i < _m; i++) {
+				for (unsigned int j = 0; j < _m; j++) {
+					unsigned int pos = i * _m + j;
+					std::vector<T> stencil = calculateSTENCIL(i, j);
+					_stencils[pos] = stencil;
+					insertRESIDUAL(pos, calculateRESIDUAL(stencil));
+				}
+			}
+
+			solve();
+
+		} while (stop());
+
+		updateSurface();
+	}
+	//Calculates the arithmetic mean over the boundary values. The result are the values of the start vector for the newton iteration:
+	T calculate_arithmetic_mean() {
+		T result = 0;
+#pragma omp parallel for reduction(+:result)
+		for (int i = 0; i < (_s - 1); i++) {
+			result += _surface[0][i] + _surface[i][0] + _surface[_s - 1][i] + _surface[i][_s - 1];
+		}
+		result /= 4 * (_s - 1);
+		return result;
+	}
+
+	void output() {
+#ifdef VTK
+		vtkSmartPointer<vtkPolyData> polydata = vtkSmartPointer<vtkPolyData>::Take(custom_reader());
+
+		vtkSmartPointer<vtkXMLPolyDataWriter> writer = vtkSmartPointer<vtkXMLPolyDataWriter>::New();
+#if VTK_MAJOR_VERSION <= 5
+		writer->SetInput(polydata);
+#else
+		writer->SetInputData(polydata);
+#endif
+		writer->SetFileName(_path.c_str());
+		writer->Write();
+#else
+		std::ofstream out(_path);
+		for (unsigned int i = 0; i < _surface.size(); i++) {
+			for (unsigned int j = 0; j < _surface[i].size(); j++) {
+				out << _surface[i][j] << " ";
+			}
+			out << std::endl;
+		}
+#endif // VTK
+	}
+
+private:
+	void updateSurface() {
+#pragma omp parallel for
+		for (int i = 1; i < _s - 1; i++) {
+			for (unsigned int j = 1; j < _s - 1; j++) {
+				unsigned int pos = (i - 1) * _m + (j - 1);
+				_surface[i][j] = getZ(pos);
+			}
+		}
+	}
+
+#ifdef VTK
+	vtkPolyData * custom_reader() {
+		vtkIdType number_of_points, number_of_triangles, N;
+		N = _s;
+		number_of_points = N * N;
+		number_of_triangles = 2 * (N - 1)*(N - 1);
+		vtkSmartPointer<vtkPoints> points
+			= vtkSmartPointer<vtkPoints>::New();
+		points->SetNumberOfPoints(number_of_points);
+
+		//write points
+		for (vtkIdType i = 0; i < N; i++) {
+			for (vtkIdType j = 0; j < N; j++) {
+				double x, y, z;
+				x = i * _h;
+				y = j * _h;
+				z = _surface[i][j];
+				points->SetPoint(i*N + j, x, y, z);
+			}
+		}
+
+		//add triangles to cells
+		vtkSmartPointer<vtkCellArray> polys
+			= vtkSmartPointer<vtkCellArray>::New();
+		for (vtkIdType i = 0; i < N - 1; i++)
+		{
+			for (vtkIdType j = 0; j < N - 1; j++)
+			{
+				polys->InsertNextCell(3);
+				polys->InsertCellPoint(i*N + j);
+				polys->InsertCellPoint(i*N + j + 1);
+				polys->InsertCellPoint((i + 1)*N + j + 1);
+
+				polys->InsertNextCell(3);
+				polys->InsertCellPoint(i*N + j);
+				polys->InsertCellPoint((i + 1)*N + j);
+				polys->InsertCellPoint((i + 1)*N + j + 1);
+			}
+		}
+
+		vtkPolyData * polydata = vtkPolyData::New();
+		polydata->SetPoints(points);
+		polydata->SetPolys(polys);
+		return polydata;
+	}
+#endif // VTK
+
+	void read_vtp(char * filename) {
+#ifdef VTK
+		const char* name = filename;
+		size_t l = strlen(name);
+		if (name[l - 4] != '.' || name[l - 3] != 'v' || name[l - 2] != 't' || name[l - 1] != 'p') {
+			std::stringstream tmp;
+			tmp << "WRONG FILE FORMAT - USE MINSURF OUTPUT FILE!";
+			throw tmp.str();
+		}
+		// Read the file
+		double timestamp = omp_get_wtime();
+		
+		vtkSmartPointer<vtkXMLPolyDataReader> reader =
+			vtkSmartPointer<vtkXMLPolyDataReader>::New();
+		
+		reader->SetFileName(filename);
+		reader->Update();
+
+		std::cout << "READING '" << filename << "' ." << std::flush;
+
+		// Extract the polydata
+		vtkSmartPointer<vtkPolyData> polydata =
+			reader->GetOutput();
+		std::cout << "." << std::flush;
+
+		// Get the number of points in the polydata
+		unsigned int idNumPointsInFile = polydata->GetNumberOfPoints();
+		_s = std::sqrt(idNumPointsInFile);
+		
+		_surface.resize(_s);
+
+		for (unsigned int i = 0; i < _s; i++) {
+			_surface[i].resize(_s);
+			for (unsigned int j = 0; j < _s; j++) {
+				double p[3];
+				polydata->GetPoint(i*_s + j, p);
+				_surface[i][j] = p[2];
+			}
+		}
+		std::cout << ". TOOK (" << omp_get_wtime() - timestamp << "s)" << std::endl;
+#else
+		throw std:string("NOT IMPLEMENTED!");
+		return;
+#endif // VTK
+	}
+};
